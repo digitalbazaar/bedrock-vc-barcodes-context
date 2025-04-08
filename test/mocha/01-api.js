b690f4ab5f3074182e76d3bf9b58cad04e2b7ccd
David I. Lehn <dlehn@digitalbazaar.com>
2025-04-07 21:45:02 -0400

    Update copyright dates.
    Co-authored-by: Dave Longley <dlongley@digitalbazaar.com>

--- test/mocha/01-api.js
+++ test/mocha/01-api.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2020-2023 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2020-2025 Digital Bazaar, Inc. All rights reserved.
  */
 import {documentLoader} from '@bedrock/jsonld-document-loader';
 
