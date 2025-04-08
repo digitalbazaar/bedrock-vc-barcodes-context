1eaefcba74d19fad05b580be7de08618aaac1f42
David I. Lehn <dlehn@digitalbazaar.com>
2025-04-07 21:13:56 -0400

    Initial files.

---
+++ test/test.js
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) 2025 Digital Bazaar, Inc. All rights reserved.
+ */
+import * as bedrock from '@bedrock/core';
+import '@bedrock/vc-barcodes-context';
+import '@bedrock/test';
+
+bedrock.start();
